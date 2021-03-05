7682b6a8f24d6fdd5a244ad4c4c688369465a469
exe32 <w.jurczyga@gmail.com>
2021-03-05 20:42:28 +0100

    Providing answers up to question 95

--- solutions.py
+++ solutions.py
@@ -1238,6 +1238,61 @@ def q90():
     a = [[[0 for i in range(8)] for j in range(5)] for k in range(3)]
     print(a)
 
-q90()
+# q90()
+
+def q91():
+    """By using list comprehension, please write a program to print the list after removing the 0th,4th,5th numbers in [12,24,35,70,88,120,155]."""
+    l = [12,24,35,70,88,120,155]
+    l = [v for i, v in enumerate(l) if i not in [0, 4, 5]]
+    print(l)
+
+# q91()
+
+def q92():
+    """ By using list comprehension, please write a program to print the list after removing the value 24 in [12,24,35,24,88,120,155]. """ 
+    l = [12,24,35,70,88,120,155]
+    l.remove(24)
+    print(l)
+
+# q92()
+
+def q93():
+    """With two given lists [1,3,6,78,35,55] and [12,24,35,24,88,120,155], write a program to make a list whose elements are intersection of the above given lists."""
+    l = [1,3,6,78,35,55]
+    k = [12,24,35,24,88,120,155]
+
+    print(list(set(l).intersection(k)))
+
+# q93()
+
+def q94():
+    """ With a given list [12,24,35,24,88,120,155,88,120,155], write a program to print this list after removing all duplicate values with original order reserved. """ 
+    l = [12,24,35,24,88,120,155,88,120,155]
+    k = []
+    for i in l:
+        if i not in k:
+            k.append(i)
+    print(k)
+
+# q94()
+
+def q95():
+    """Define a class Person and its two child classes: Male and Female. All classes have a method "getGender" which can print "Male" for Male class and "Female" for Female class."""
+    class Person():
+        pass
+    class Male(Person):
+        @staticmethod
+        def getGender():
+            print("Male")
+
+    class Female(Person):
+        @staticmethod
+        def getGender():
+            print("Female")
+    
+    Female.getGender()
+
+q95()
+
 
 doctest.testmod()
